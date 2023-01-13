8d2e4026ee30f7f2b18c719ef9fbbcf3bcd7158e
karthik-jeevith <ckarthikmuthu1234@gmail.com>
2023-01-13 20:57:28 +0530

    Added Rest controller using JPA and H2 Db

---
+++ src/main/java/com/exceptionhandlingjava/exceptionrestapi/services/UserService.java
@@ -0,0 +1,55 @@
+package com.exceptionhandlingjava.exceptionrestapi.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.exceptionhandlingjava.exceptionrestapi.dto.UserRequest;
+import com.exceptionhandlingjava.exceptionrestapi.entity.User;
+import com.exceptionhandlingjava.exceptionrestapi.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(UserRequest userRequest) {
+
+        User user = User.builder()
+                .name(userRequest.getName())
+                .email(userRequest.getEmail())
+                .mobile(userRequest.getMobile())
+                .gender(userRequest.getGender())
+                .age(userRequest.getAge())
+                .nationality(userRequest.getNationality()).build();
+
+
+        // Normal way of assinging values into the object.
+        // User user1 = new User();
+        // user1.setName(userRequest.getName());
+        // user1.setEmail(userRequest.getEmail());
+        // user1.setMobile(userRequest.getMobile());
+        // user1.setGender(userRequest.getGender());
+        // user1.setAge(userRequest.getAge());
+        // user1.setNationality(userRequest.getNationality());
+
+
+        return userRepository.save(user);
+
+    }
+
+
+    public List<User> findAlluser()
+    {
+        return userRepository.findAll();
+    }
+
+    public User findUser(int Id){
+
+        // create custom method findBy followed by entity/model property in JPA. ex:findBy+ userId(model property)
+        return userRepository.findByuserId(Id);
+    }
+
+}
